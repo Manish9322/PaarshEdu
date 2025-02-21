c8d6ec9f03c39bfb8246229b008054ea95573282
Ashwini_Bhavare <ashwinibhavare05@gmail.com>
2025-02-21 15:25:08 +0530

    Implemented: About

--- src/components/About/AboutSectionTwo.tsx
+++ src/components/About/AboutSectionTwo.tsx
@@ -5,7 +5,7 @@ const AboutSectionTwo = () => {
     <section className="py-16 md:py-20 lg:py-28">
       <div className="container">
         <div className="-mx-4 flex flex-wrap items-center">
-          <div className="w-full px-4 lg:w-1/2">
+          <div className="w-full px-4 lg:w-1/3">
             <div
               className="relative mx-auto mb-12 aspect-[25/24] max-w-[500px] text-center lg:m-0"
               data-wow-delay=".15s"
@@ -24,33 +24,68 @@ const AboutSectionTwo = () => {
               />
             </div>
           </div>
-          <div className="w-full px-4 lg:w-1/2">
+          <div className="w-full px-8 lg:w-1/3">
             <div className="max-w-[470px]">
               <div className="mb-9">
                 <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
-                  Bug free code
+                  Remote Classrooms
                 </h3>
                 <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Run and Manage Interactive webinars to permit a personalized
+                  learning experience.
                 </p>
               </div>
               <div className="mb-9">
                 <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
-                  Premier support
+                  Course Manager
                 </h3>
                 <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt.
+                  Manage the courses your learners study from different sources
+                  and follow them to their individual needs.
                 </p>
               </div>
               <div className="mb-1">
                 <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
-                  Next.js
+                  Paarsh E-Learn
                 </h3>
                 <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
-                  Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt
-                  consectetur adipiscing elit setim.
+                  Provide video, documents, or external links like Youtube-based
+                  internal training to complete the pieces of training.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          {/* Third Section - Text Content 2 */}
+          <div className="w-full px-8 lg:w-1/3">
+            <div className="max-w-[470px]">
+              <div className="mb-9">
+                <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
+                  Scalable Pricing
+                </h3>
+                <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
+                  Customize our flexible pricing strategies to suit our
+                  learners needs and budget.
+                </p>
+              </div>
+              <div className="mb-9">
+                <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
+                  Certificates
+                </h3>
+                <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
+                  Automatically add achievement certificates to the learner’s
+                  education tracker whether studied online, offline, or via
+                  Google Meet.
+                </p>
+              </div>
+
+              <div className="mb-9">
+                <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
+                  24/7 Support
+                </h3>
+                <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
+                  Create an in-person or a virtual classroom session and track
+                  the attendance by Online Google Sheet.
                 </p>
               </div>
             </div>
