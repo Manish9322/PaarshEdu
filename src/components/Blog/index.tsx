9e8ebcf4fa263a7ca30db7de325895bb04ac7c93
Ashwini_Bhavare <ashwinibhavare05@gmail.com>
2025-02-22 17:32:37 +0530

    contact page and blog page edited

--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -14,9 +14,8 @@ const Blog = () => {
           paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
           center
         />
-
-        <div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 md:gap-x-6 lg:gap-x-8 xl:grid-cols-3">
-          {blogData.map((blog) => (
+<div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 md:gap-x-6 lg:gap-x-8 xl:grid-cols-3">
+          {blogData.slice(0, 3).map((blog) => (
             <div key={blog.id} className="w-full">
               <SingleBlog blog={blog} />
             </div>
